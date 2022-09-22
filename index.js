da0792e00e2b406f43540ae33c8a86c9626c5eef
Gabriel Moraes <gmoraes.mat@gmail.com>
2022-09-22 01:29:35 -0300

    preparação e configuração do ambiente

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+
+
+
+
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000')
+})
